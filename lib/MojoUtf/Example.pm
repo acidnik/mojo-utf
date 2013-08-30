4f9ffe29d0b681823914ec01327c94f6389e6b34
Nik Bilous <nikita.bilous@gmail.com>
2013-08-30 15:29:51 +0400

    init

---
+++ lib/MojoUtf/Example.pm
@@ -0,0 +1,22 @@
+package MojoUtf::Example;
+use Mojo::Base 'Mojolicious::Controller';
+use DBI;
+use utf8;
+
+# This action will render a template
+sub welcome {
+  my $self = shift;
+
+  my $dbh = DBI->connect("DBI:mysql:database=test;mysql_enable_utf8=1");
+
+  $dbh->do("create table if not exists foo (id serial, name varchar(100) not null unique) default charset = utf8");
+  $dbh->do("insert ignore into foo (name) values (?)",undef, "великий и могучий");
+
+  my $row = $dbh->selectrow_hashref("select name from foo limit 1");
+
+
+  # Render template "example/welcome.html.ep" with message
+  $self->render(message => $row->{name});
+}
+
+1;
